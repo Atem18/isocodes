c943229a0213ed2d1d66ee59523a3a1cde32d4d0
Atem18 <messer.kevin@gmail.com>
2021-12-21 23:09:22 +0100

    Rebase from upstream

--- update.sh
+++ update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-BASE_DIR=$PWD/vendor/iso-codes
+BASE_DIR="$PWD/isocodes"
 
-cd "$BASE_DIR" || exit
+SHARE_DIR="$PWD"/isocodes/share
+
+UPSTREAM_DIR=$PWD/vendor/iso-codes
+
+rm -rf "$SHARE_DIR"
+
+cd "$UPSTREAM_DIR" || exit
 
 git pull origin main
 
-./configure "$BASE_DIR"
+./configure --prefix "$BASE_DIR"
 
 make clean
 
