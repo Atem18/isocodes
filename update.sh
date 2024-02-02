89abfd7b01187520ed83cbf0fa7de07daeef94a1
Atem Serem <contact@atemlire.com>
2024-02-02 10:02:26 +0000

    Bump iso-codes and isocodes versions

--- update.sh
+++ update.sh
@@ -8,11 +8,13 @@ UPSTREAM_DIR=$PWD/vendor/iso-codes
 
 rm -rf "$SHARE_DIR"
 
+rm -rf "$UPSTREAM_DIR"
+
+git submodule update --init --recursive
+
 cd "$UPSTREAM_DIR" || exit
 
-git pull
-
-git checkout v4.15.0
+git checkout v4.16.0
 
 ./configure --prefix "$BASE_DIR"
 
